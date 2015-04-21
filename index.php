29ea30daa35fb26d5eefcbd1a04de1ccfbba5e18
Joshua Smith <joshsmith0419@gmail.com>
2015-04-21 09:10:17 -0700

    Milestone 1 & 2

---
+++ index.php
@@ -0,0 +1,12 @@
+<<!DOCTYPE html>
+<html>
+<head>
+	<title>Joshua's T-Do List #2</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+	<div class="wrap">  
+	
+	</div>
+</body>
+</html>
